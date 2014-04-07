76a81395bf73a7b0449fffb0add71613c472ed47
Subin Siby <subins2000@gmail.com>
2014-04-07 22:33:01 +0530

    Good

--- .openshift/cron/minutely/crawler.php
+++ .openshift/cron/minutely/crawler.php
@@ -1,5 +1,4 @@
 <?
 $crawlToken=418941;
-set_include_path('/var/lib/openshift/53400ee04382ec99f6000125/app-root/runtime/repo');
-include("crawler/crawl.php");
+include("/var/lib/openshift/53400ee04382ec99f6000125/app-root/runtime/repo/crawler/crawl.php");
 ?>
