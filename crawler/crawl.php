8487f82b7a3039afcc2bd3a3cb5e0bd982acb601
Subin Siby <subins2000@gmail.com>
2014-04-07 12:00:25 +0530

    Good

--- crawler/crawl.php
+++ crawler/crawl.php
@@ -1,33 +1,32 @@
 <?
 ini_set("display_errors", "on");
 include("PHPCrawl/libs/PHPCrawler.class.php");
-$GLOBALS['maxtime']=strtotime("+10 second");
+$GLOBALS['maxtime']=strtotime("+1 second");
 $GLOBALS['crawled']=array();
 class WSCrawler extends PHPCrawler { 
  function handleDocumentInfo(PHPCrawlerDocumentInfo $p) { 
   $u=$p->url;
-  if($GLOBALS['maxtime'] > time()){
-   crawlAdd($u);
-  }
+  crawlInit($u);
  }
 }
-function crawlAdd($u){
+function crawlInit($u){
  $uen=urlencode($u);
- if(array_search($uen, $GLOBALS['crawled'])===false){
+ if(array_search($uen, $GLOBALS['crawled'])===false && $GLOBALS['maxtime'] > time()){
   $GLOBALS['crawled'][]=$uen;
-  $C = new WSCrawler();
-  $C->setURL($u);
-  $C->addContentTypeReceiveRule("#text/html#");
-  $C->addURLFilterRule("#(jpg|gif|png|pdf|jpeg|svg|css|js)$# i");
-  $C->setPageLimit(100, true);
-  $C->obeyRobotsTxt(true);
-  $C->setUserAgentString("Dingo Bot (http://search.subinsb.com/about/bot.php)");
-  $C->setFollowMode(0);
-  $C->force_output_flushing = true;
   echo $u."<br/>";
-  flush();
-  $C->go();
+  crawlNow($u);
  }
 }
-crawlAdd("http://www.google.com");
+function crawlNow($u){
+ $C = new WSCrawler();
+ $C->setURL($u);
+ $C->addContentTypeReceiveRule("#text/html#");
+ $C->addURLFilterRule("#(jpg|gif|png|pdf|jpeg|svg|css|js)$# i");
+ $C->setPageLimit(100, true);
+ $C->obeyRobotsTxt(true);
+ $C->setUserAgentString("Dingo Bot (http://search.subinsb.com/about/bot.php)");
+ $C->setFollowMode(1);
+ $C->go();
+}
+crawlInit("http://www.google.com");
 ?>
