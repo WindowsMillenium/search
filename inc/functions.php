550c80ddd8ab927feca494be54f28a2a1f11abe3
Subin Siby <subins2000@gmail.com>
2014-04-08 11:38:07 +0530

    Added StatCounter Stats

--- inc/functions.php
+++ inc/functions.php
@@ -36,9 +36,7 @@ function headerElem(){ // header() is already a function in PHP
  echo $header;
 }
 function footer(){
- $footer = '<script type="text/javascript">
-var sc_project=9729182; var sc_invisible=1; var sc_security="f7e9a3d1"; var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");document.write("<sc"+"ript type=\'text/javascript\' src=\'" + scJsHost+ "statcounter.com/counter/counter.js\'></"+"script>");</script>
-<noscript><div class="statcounter" style="display:none;"><img class="statcounter" src="http://c.statcounter.com/9729182/0/f7e9a3d1/1/" alt="hits counter"></a></div></noscript>';
+ $footer = include("track.php");
  $footer .= "<div class='footer'><a href='".HOST."/about'>About</a><a href='".HOST."/about/stats.php'>Stats</a><a href='".HOST."/about/bot.php'>Dingo</a><div style='float:right;'>&copy; Copyright Subin ".date("Y")."</div></div>";
  $footer.='
  <svg style="display:none;">
