0f6d792dc0ea4a48814bd6f8230be60066df2fd2
Subin Siby <subins2000@gmail.com>
2014-04-08 21:32:09 +0530

    Added Spell Check

--- inc/spellcheck.php
+++ inc/spellcheck.php
@@ -5,7 +5,7 @@ class SpellCheck{
   return true;
  }
  private function makeURL($s){
-  $s=str_replace(" ", "%20", $s);
+  $s=urlencode($s);
   $url=$this->url."?client=t&sl=en&tl=en&hl=en&sc=2&ie=UTF-8&oe=UTF-8&uptl=en&oc=1&otf=1&ssel=3&tsel=0";
   $url.="&q=$s";
   return $url;
@@ -13,6 +13,7 @@ class SpellCheck{
  public function check($s){
   $a="";
   $c=file_get_contents($this->makeURL($s));
+  $c=substr_replace($c, "", 0, 41);
   preg_match('/u003e","(.*?)",\[1]/', $c, $m);
   if(isset($m[1])){
    $a=$m[1];
