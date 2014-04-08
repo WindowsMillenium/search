45b3cbe1ca27e96fb237569ce4bcbc725b66a13f
Subin Siby <subins2000@gmail.com>
2014-04-08 11:48:52 +0530

    Added StatCounter Stats

--- inc/track.php
+++ inc/track.php
@@ -1,1 +1,7 @@
-'<script type="text/javascript">var sc_project=9729182; var sc_invisible=1; var sc_security="f7e9a3d1"; var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");document.write("<sc"+"ript type=\'text/javascript\' src=\'" + scJsHost+ "statcounter.com/counter/counter.js\'></"+"script>");</script><noscript><div class="statcounter" style="display:none;"><img class="statcounter" src="http://c.statcounter.com/9729182/0/f7e9a3d1/1/" alt="hits counter"></a></div></noscript>'
+<?
+if(HOST=="http://search.subinsb.com"){
+?>
+<script type="text/javascript">var sc_project=9729182; var sc_invisible=1; var sc_security="f7e9a3d1"; var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");document.write("<sc"+"ript type=\'text/javascript\' src=\'" + scJsHost+ "statcounter.com/counter/counter.js\'></"+"script>");</script><noscript><div class="statcounter" style="display:none;"><img class="statcounter" src="http://c.statcounter.com/9729182/0/f7e9a3d1/1/" alt="hits counter"></a></div></noscript>
+<?
+}
+?>
