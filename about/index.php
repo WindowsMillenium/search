3fcad9acaa6d82991ee02c23e286a61b0f89a9f7
SIMSU <subins2000@gmail.com>
2014-04-20 12:15:20 +0530

    No Time Limit for crawler

--- about/index.php
+++ about/index.php
@@ -7,8 +7,8 @@
   <?headerElem();?>
   <div class="container" style="width:300px;">
    <h2>About</h2>
-   <p>Web Search is a simple software written in PHP that<br/>functions as a search engine. It's called as WS</p>
-   <p>WS's crawler is named Dingo. It will crawl 100 pages<br/>each minute and indexes them.</p>
+   <p>Web Search is a simple software written in PHP that functions as a search engine. It's called <b>WS</b>.</p>
+   <p>WS's crawler is named Dingo. It will crawl about 100 pages each minute and indexes them.</p>
    <p>WS don't store any cookies and is perfectly safe.</p>
    <h2>Key features</h2>
    <ul>
@@ -17,7 +17,8 @@
     <li>No Cookies</li>
     <li>No Malcious Tracking</li>
    </ul>
-   <p>Help Me to keep creating projects :-)</p><br/>
+   <p>Site Stats are tracked using StatCounter. You can see it <a target="_blank" href="http://statcounter.com/p9729182/summary/?guest=1">here</a>.</p>
+   <p>Donate if you like my work</p><br/>
    <center><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="ZYQWUZ2B8ZXXA"><button name="submit" type="submit"><img alt="Donate" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"></button><img alt="Donate" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1" border="0"></form></center>
   </div>
   <?footer();?>
