ac6a3f3fea2a8afca994ab291dbd692740879f15
SIMSU <subins2000@gmail.com>
2014-04-28 10:18:39 +0530

    Redirect By JavaScript

--- url.php
+++ url.php
@@ -4,7 +4,14 @@ if(filter_var($url, FILTER_VALIDATE_URL) === FALSE || $url==""){
  header("Location: http://".$_SERVER['HTTP_HOST'], 302);
  exit;
 }else{
- header("Location: ".$url);
- exit;
+?>
+ <html>
+  <head>
+   <noscript><META http-equiv="refresh" content="0;URL=<?echo$url;?>"></noscript>
+   <script>window.location.replace("<?echo$url;?>");</script>
+  </head>
+  <body></body>
+ </html>
+<?
 }
 ?>
